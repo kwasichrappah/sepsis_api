69de80da565e7d8501b7adc6c2817f68274702d5
chrappah <chrappahkwasi@gmail.com>
2024-07-08 22:10:03 +0000

    Create api.py

---
+++ api/api.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from typing import Union
+import joblib
+import pandas as pd
+from pydantic import BaseModel
+
+'''to run the API, run this line which is based on the api directory then 'uvicorn api(api python file):app(instance of fast API) --reload' '''
+# Create a FastAPI application
+app = FastAPI()
+
+class patient_features(BaseModel):
+	PRG :float
+	PL :float
+	PR :float
+	SK :float
+	TS :float
+	M11 :float
+	BD2 :float
+	Age :int
+
+# Define a route at the root web address ("/")
+@app.get("/")
+def status_check():
+	return {"Status": "API is online!!!"}
+
+
+pipeline = joblib.load('C:\\Users\\chrap\\OneDrive - ECG Ghana\\Emmanuel Chrappah\\Azubi Africa\\git_hub_repos\\sepsis_ml_predictor\\models\\best_gs_model.joblib')
+#encoder = joblib.load('')
+
+@app.post("/prediction")
+def predict_sepssis(data:patient_features):
+
+    df = pd.DataFrame([data.model_dump()])
+    pipeline.predict(df)
+
+	#return {"All docus": "API Documentation"}
