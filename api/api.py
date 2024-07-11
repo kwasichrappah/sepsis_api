cdc821fd89c9ac42d675c3342ffacd64d516bd14
chrappah <chrappahkwasi@gmail.com>
2024-07-11 16:21:34 +0000

    update

--- api/api.py
+++ api/api.py
@@ -1,40 +1,54 @@
 from fastapi import FastAPI
 # from typing import Union
+
+#Filter warnings
+import warnings
+warnings.filterwarnings('ignore')
+
 import joblib
 import pandas as pd
 from pydantic import BaseModel
 
+from sklearn.externals import joblib
 
-pipeline = joblib.load('./models/best_gs_model.joblib')
-#encoder = joblib.load('')
+print(joblib.__version__)
 
-'''to run the API, run this line which is based on the api directory then 'uvicorn api(api python file):app(instance of fast API) --reload' '''
-# Create a FastAPI application
-app = FastAPI()
-
-class patient_features(BaseModel):
-	PRG :float
-	PL :float
-	PR :float
-	SK :float
-	TS :float
-	M11 :float
-	BD2 :float
-	Age :int
-
-# Define a route at the root web address ("/")
-@app.get("/")
-def status_check():
-	return {"Status": "API is online!!!"}
+model = joblib.load("/models/best_gs_model.joblib")
+    
 
 
+#pipeline = joblib.load(r"/models/best_gs_model.joblib")
+#print(pipeline)
+# # #encoder = joblib.load('')
 
 
-@app.post("/prediction")
-def predict_sepssis(data:patient_features):
+# '''to run the API, run this line which is based on the api directory then 'uvicorn api(api python file):app(instance of fast API) --reload' '''
+# # Create a FastAPI application
+# app = FastAPI()
 
-    df = pd.DataFrame([data.model_dump()])
-    #pipeline.predict(df)
+# class patient_features(BaseModel):
+# 	PRG :float
+# 	PL :float
+# 	PR :float
+# 	SK :float
+# 	TS :float
+# 	M11 :float
+# 	BD2 :float
+# 	Age :int
 
-	#return {"All docus": "API Documentation"}
+# # Define a route at the root web address ("/")
+# @app.get("/")
+# def status_check():
+# 	return {"Status": "API is online!!!"}
+
+
+
+
+# @app.post("/prediction")
+# def predict_sepssis(data:patient_features):
+
+#     df = pd.DataFrame([data.model_dump()])
+#     #pipeline.predict(df)
+
+# 	#return {"All docus": "API Documentation"}
 
