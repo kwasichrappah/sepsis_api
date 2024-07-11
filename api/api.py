da3d37c9951ab1ad351ecb4378e0b1f805e016bf
chrappah <chrappahkwasi@gmail.com>
2024-07-11 18:25:58 +0000

    api_update

--- api/api.py
+++ api/api.py
@@ -9,46 +9,44 @@ import joblib
 import pandas as pd
 from pydantic import BaseModel
 
-from sklearn.externals import joblib
+import pickle
 
-print(joblib.__version__)
-
-model = joblib.load("/models/best_gs_model.joblib")
-    
+#favorite_color = pickle.load( open( "./api/best_gs_model.pkl", "rb" ) )
 
 
-#pipeline = joblib.load(r"/models/best_gs_model.joblib")
+
+pipeline = joblib.load(r"..//api//best_gs_model.joblib")
 #print(pipeline)
 # # #encoder = joblib.load('')
 
 
-# '''to run the API, run this line which is based on the api directory then 'uvicorn api(api python file):app(instance of fast API) --reload' '''
-# # Create a FastAPI application
-# app = FastAPI()
+'''to run the API, run this line which is based on the api directory then 'uvicorn api(api python file):app(instance of fast API) --reload' '''
+# Create a FastAPI application
+app = FastAPI()
 
-# class patient_features(BaseModel):
-# 	PRG :float
-# 	PL :float
-# 	PR :float
-# 	SK :float
-# 	TS :float
-# 	M11 :float
-# 	BD2 :float
-# 	Age :int
+class patient_features(BaseModel):
+	PRG :float
+	PL :float
+	PR :float
+	SK :float
+	TS :float
+	M11 :float
+	BD2 :float
+	Age :int
 
-# # Define a route at the root web address ("/")
-# @app.get("/")
-# def status_check():
-# 	return {"Status": "API is online!!!"}
-
-
+# Define a route at the root web address ("/")
+@app.get("/")
+def status_check():
+	return {"Status": "API is online!!!"}
 
 
-# @app.post("/prediction")
-# def predict_sepssis(data:patient_features):
 
-#     df = pd.DataFrame([data.model_dump()])
-#     #pipeline.predict(df)
 
-# 	#return {"All docus": "API Documentation"}
+@app.post("/prediction")
+def predict_sepssis(data:patient_features):
+
+    df = pd.DataFrame([data.model_dump()])
+    #pipeline.predict(df)
+
+	#return {"All docus": "API Documentation"}
 
